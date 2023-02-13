5621068ad82c09b49e5ae5d522fde3a8414b23c9
kazuki <klklklkl063055@gmail.com>
2023-02-13 13:29:48 +0900

    tried to plot

--- get_price.py
+++ get_price.py
@@ -1,14 +1,21 @@
 # import libraries
-# import pandas_datareader.data as pdr
 import datetime as dt
 import yfinance as yf 
 
 # import to visualize
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 # import seaborn as sns
 # sns.set
+
+# get prices
 code = 'SPY AAPL'
 start = dt.date(2023,1,1)
 end = dt.date.today()
 data = yf.download(code, start=start, end=end)
 print(data.head())
+
+# plot
+data.plot(figsize=(8,6),fontsize=18)
+plt.legend(bbox_to_anchor=(0, 1), loc='upper left', borderaxespad=1, fontsize=18)
+plt.grid(True)
+plt.show()
