04b2fce915a7963268e69fffce931865967b3a0a
kazuki <klklklkl063055@gmail.com>
2023-02-13 13:17:40 +0900

    display prices using yahoo finance

--- get_price.py
+++ get_price.py
@@ -1,7 +1,14 @@
 # import libraries
-import pandas_datareader.data as pdr
-import datetime
+# import pandas_datareader.data as pdr
+import datetime as dt
+import yfinance as yf 
+
 # import to visualize
-from matplotlib import pyplot as plt
-import seaborn as sns
-sns.set
+# from matplotlib import pyplot as plt
+# import seaborn as sns
+# sns.set
+code = 'SPY AAPL'
+start = dt.date(2023,1,1)
+end = dt.date.today()
+data = yf.download(code, start=start, end=end)
+print(data.head())
