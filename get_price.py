52de8e19b4716511db663814859f4a21ac7529f7
kazuki <klklklkl063055@gmail.com>
2023-02-20 00:19:42 +0900

    small modify to simplify

--- get_price.py
+++ get_price.py
@@ -1,14 +1,11 @@
 # import libraries
-import datetime as dt
 import yfinance as yf 
 
 # get prices
-codelist = ["AAPL","AMZN","MSFT","GOOGL"]
-start = dt.date(2020,1,1)
-end = dt.date.today()
-data = yf.download(codelist, start=start, end=end)["Adj Close"]
-print(data.head().append(data.tail()))
+def make_dataframe(codes, start, end, value):
+    data = yf.download(codes, start=start, end=end)[value]
 
-# convert to percentage ( to compare by same criteria )
-df_all=(1+data.pct_change()).cumprod()
-print(df_all.head().append(df_all.tail()))
+    # convert to percentage ( to compare by same criteria )
+    df_all = (1+data.pct_change()).cumprod()
+    return df_all
+
