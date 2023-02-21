31c9c5b577b7ff3d06258595427fe165b400283f
kazuki <klklklkl063055@gmail.com>
2023-02-22 03:15:04 +0900

    apply OOP

--- get_price.py
+++ get_price.py
@@ -1,11 +1,28 @@
 # import libraries
 import yfinance as yf 
+# import to visualize
+from matplotlib import pyplot as plt
 
-# get prices
-def make_dataframe(codes, start, end, value):
-    data = yf.download(codes, start=start, end=end)[value]
+class Analyse_Data():
 
-    # convert to percentage ( to compare by same criteria )
-    df_all = (1+data.pct_change()).cumprod()
-    return df_all
+    def __init__(self, start, end, value) -> None:
+        self.codes = []
+        self.start = start
+        self.end = end
+        self.value = value
+
+    # get data
+    def make_dataframe(self):
+        data = yf.download(self.codes, start=self.start, end=self.end)[self.value]
+        # convert to percentage ( to compare by same criteria )
+        df_all = (1+data.pct_change()).cumprod()
+        return df_all
+    
+    # plot
+    def plot_data(self):
+        df_all = self.make_dataframe()
+        df_all.plot(figsize=(8,6),fontsize=18)
+        plt.legend(bbox_to_anchor=(0, 1), loc='upper left', borderaxespad=1, fontsize=18)
+        plt.grid(True)
+        plt.show()
 
---
+++ main.py
@@ -0,0 +1,6 @@
+# import 
+import datetime as dt
+
+# execute
+
+
